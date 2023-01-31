0ed77407171e39b8de803e565eeccfdb8582d92b
Hyrox_ <61850271+TheHyrox@users.noreply.github.com>
2023-01-31 18:51:07 +0100

    Track mario with lua

--- marioia.lua
+++ marioia.lua
@@ -16,9 +16,11 @@ buttons = {
 }
 
 function getMarioPos()
-	local mario = {}
-	mario.x = memory.readbyte(0x86) + memory.readbyte(0x6D)*0x100 + 4
-	mario.y = memory.readbyte(0x3B8)
+	local mario = {} 
+	mario.X = memory.readbyte(0x86) + memory.readbyte(0x6D)*0x100 + 4
+	mario.Y = memory.readbyte(0x3B8)
+	mario.x = memory.readbyte(0x217) 
+	mario.y = memory.read_s16_le(0x3B8)
 	return mario
 end
 
@@ -36,14 +38,25 @@ end
 function drawinfo()
 	local mario = getMarioPos()
 	gui.drawBox(0, 0, 256, 40, "black", "white")
-	gui.drawText(0, 4,"Mario posX : " .. mario.x .. " Mario posY : " .. mario.y .. "\nMario goes to " .. getMarioFacing(), "black")
+	gui.drawText(0, 4,"Mario posX : " .. mario.X .. " Mario posY : " .. mario.Y .. "\nMario goes to " .. getMarioFacing(), "black")
+end
+
+function drawmap()
+	
+end
+function drawmario()
+	local mario = getMarioPos()
+	gui.drawRectangle(mario.x + 5, mario.Y + 10, 6, 6 * 2, "black", "blue")
 end
 
 while true do
 	gui.clearGraphics()
 	drawinfo(getMarioFacing())
+	drawmario()
 	emu.frameadvance()
 end
+
+
 
 --Y = 0 quand MposY = 176, 160 = 1 bloc, 144, 128, 112, 96, 80, 64, 48
 -- screen size 256, 223
